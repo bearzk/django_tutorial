70080cf08487a70880e8ecf656b664a727a2e369
bearzk <kai.zhangxa@gmail.com>
2014-12-08 00:15:00 +0100

    create new post from index page

--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,19 @@ import re
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.views.generic import ListView
-from models import Post, Author
+from django.views.generic import CreateView
+from models import Post, Author, Comment
 
 
-class IndexView(ListView):
+class IndexView(CreateView):
     template_name = 'blog/index.html'
     context_object_name = 'ps'
 
-    def get_queryset(self):
-        return Post.objects.order_by('-pub_date')[:5]
+    def get(self, request, *args, **kwargs):
+        ps = Post.objects.order_by('-pub_date')[:10]
+        return render(request, 'blog/index.html', {'ps': ps})
 
-    def add(self, request):
+    def post(self, request, *args, **kwargs):
         a = get_object_or_404(Author, nickname='bearzk')
         title = request.POST['title'].strip()
         slug = re.sub('\s', '-', title.lower())
@@ -22,14 +23,26 @@ class IndexView(ListView):
         p.save()
         return redirect('blog:index')
 
-
-def detail(request, post_id):
-    p = get_object_or_404(Post, id=post_id)
+def detail(request, post_slug):
+    p = get_object_or_404(Post, slug=post_slug)
     a = get_object_or_404(Author, pk=p.author_id)
-    return render(request, 'blog/detail.html', {'p': p, 'a': a})
+    cs = Comment.objects.filter(post_id=p.id)
+    return render(request, 'blog/detail.html', {'p': p, 'a': a, 'cs': cs})
 
 
 def author(request, author_name):
     a = get_object_or_404(Author, nickname=author_name)
     return render(request, 'blog/author.html', {'a': a})
 
+
+class CommentView(CreateView):
+    template_name = 'blog/comment.html'
+    context_object_name = 'cs'
+
+    def post(self, request, *args, **kwargs):
+        p = get_object_or_404(Post, post_id=request.POST['post_id'])
+        a = get_object_or_404(Author, nickname='bearzk')
+        content = request.POST['content'].strip()
+        c = p.post_set.create(content=content, post_id=p.id, author_id=a.id)
+        c.save()
+        return redirect('detail', slug=p.slug)
