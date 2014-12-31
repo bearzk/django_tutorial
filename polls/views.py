bda34a5c75f107c0c576a10e5f2f89a29e4ecc71
bearzk <kai.zhangxa@gmail.com>
2014-12-31 23:58:22 +0100

    updated polls code, bye 2014

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('you are at polls index')
+def detail(request, question_id):
+    return HttpResponse('you are looking at question %s.' % question_id)
+
+def results(request, question_id):
+    response = 'you are looking at %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('you are looking at question %s' % question_id)
