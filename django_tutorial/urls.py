a31f42777449f9a8f3c8fef2556a45aa639a19d9
bearzk <kai.zhangxa@gmail.com>
2015-01-03 22:22:58 +0100

    polls with basic votes functionality

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,31 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+
+from polls.models import Question, Choice
 
 def index(request):
-    return HttpResponse('you are at polls index')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
 def detail(request, question_id):
-    return HttpResponse('you are looking at question %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'you are looking at %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('you are looking at question %s' % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': q,
+            'error_message': "You didn't seletc a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
