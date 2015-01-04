3038527d5be2bc078189ba16fc14196b5c447efe
bearzk <kai.zhangxa@gmail.com>
2015-01-04 20:25:04 +0100

    basic test of model

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
         return unicode(self).encode('utf-8')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
