5ce3675a59766c29b2e2063bf206a836a2e912e9
bearzk <kai.zhangxa@gmail.com>
2015-01-04 21:08:19 +0100

    index and detail view test

--- polls/tests.py
+++ polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 from polls.models import Question
 
@@ -16,3 +17,60 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=1)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text,
+                                   pub_date=time)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No question yet.", status_code=200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_indext_with_a_past_question(self):
+        create_question(question_text='from past', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: from past>']
+        )
+
+    def test_indext_with_a_future_question(self):
+        create_question(question_text='from future', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No question yet.", status_code=200)
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_indext_with_future_question_and_past_question(self):
+        create_question(question_text='from future', days=30)
+        create_question(question_text='from past', days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'],
+                                 ["<Question: from past>"])
+
+    def test_index_views_with_two_past_questions(self):
+        create_question(question_text='1st old', days=-30)
+        create_question(question_text='2nd old', days=-29)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'],
+                                 ["<Question: 2nd old>", "<Question: 1st old>"])
+
+
+class DetailViewTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        future_question = create_question(question_text='future question',
+                                          days=20)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        past_question= create_question(question_text='past question',
+                                       days=-20)
+        response = self.client.get(reverse('polls:detail',
+                                           args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text, status_code=200)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Question, Choice
 
@@ -9,13 +10,17 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
