e971bf88a68f32bc90d229316e9284951cc41505
bearzk <kai.zhangxa@gmail.com>
2015-01-18 23:13:10 +0100

    #1 removed comment url

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,5 @@ from blog import views
 urlpatterns = patterns('',
     url(r'author/(?P<author_name>[a-zA-Z0-9-_]+)$', views.author, name='author'),
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'(?P<post_slug>.*)/comment/$', views.add_comment, name='add_comment'),
     url(r'(?P<post_slug>.*)', views.detail, name='detail'),
 )
