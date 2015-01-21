b3e50281eb73467f61bc41921aa56d510f40896e
bearzk <kai.zhangxa@gmail.com>
2015-01-21 22:36:45 +0100

    logout link

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'account/login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/blog/'}),
 )
